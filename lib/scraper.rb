b31add96d89a176ac430b2c433bda6bc45fb67e9
Ali Alsayed <a_alsayed87@hotmail.com>
2018-12-01 12:27:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,19 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    #students: doc.css(".student-name")
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
+Scraper.scrape_index_page("./fixtures/student-site/index.html")
