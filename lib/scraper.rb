acd00cb91a3a5cbba59262432f82dec5f3d83427
Ali Alsayed <a_alsayed87@hotmail.com>
2018-12-01 09:19:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,16 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
